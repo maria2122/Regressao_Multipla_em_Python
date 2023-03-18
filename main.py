c734579d3965e464ce58154447977cefac92bae5
maria2122 <mariajsantos199@gmail.com>
2023-03-17 22:34:15 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, jsonify
+import pickle
+
+modelo = pickle.load(open('modelo.sav', 'rb'))
+colunas = ['comprimentoS','larguraS','comprimentoP', 'larguraP']
+
+app = Flask('meu_app')
+
+@app.route('/')
+def index():
+    return render_template('novo.html', titulo='Classificacao')
+
+@app.route('/classifica/', methods=['POST'])
+def classifica():
+    dados = request.form
+    dados_input = [float(dados[col]) for col in colunas]
+    classe = modelo.predict([dados_input])
+    if classe == 0:
+        classe='Iris-setosa'
+    elif classe == 1:
+        classe='Iris-versicolor'
+    else:
+        classe='Iris-virginica'
+    return classe
+
+app.run()
