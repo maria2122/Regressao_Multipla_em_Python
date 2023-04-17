2b443264865b0911698fd8ef571d9b2b8cc4df2e
Isaias Souza <isaias.souza.spfc@gmail.com>
2023-04-17 02:09:34 -0300

    Conexao backfront

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 from flask import Flask, render_template, request, jsonify
 import pickle
 
-modelo = pickle.load(open('modelo.sav', 'rb'))
-colunas = ['Salario_medio','Numero_Empregos','Preco_Moradia', 'Custo_Vida','Cidade','Aluguel', 'Poder_Compra']
+modelo = pickle.load(open('./static/dados/modelo.sav', 'rb'))
+colunas = [ 'TamanhoM',
+            'PesoC',
+            'Potencia', 
+            'Comprimento',
+            'Altura',
+            'TipoR',
+            'cilindros',
+            'consumoR',
+            'consumoC']
 
 app = Flask('meu_app')
 
 @app.route('/')
 def index():
-    return render_template('index.html', titulo='Classificação')
+    return render_template('index.html', titulo='Regressão')
 
-@app.route('/classifica/', methods=['POST'])
+@app.route('/classifica', methods=['POST'])
 def classifica():
     dados = request.form
     dados_input = [float(dados[col]) for col in colunas]
-    classe = modelo.predict([dados_input])
-    if classe == 0:
-        classe='Iris-setosa'
-    elif classe == 1:
-        classe='Iris-versicolor'
-    else:
-        classe='Iris-virginica'
-    return classe
+    valor = modelo.predict([dados_input])
 
-
+    return render_template('resultado.html', resultado=valor[0])
 
 if __name__ == "__main__":
     app.run(debug=True)
